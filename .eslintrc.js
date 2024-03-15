e6faa63cb29b5eec3a21f93666d0833f64087591
nolawnchairs <nolawnchairs@gmail.com>
2024-03-16 00:50:43 +0700

    rename linting project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
-    project: './tsconfig.local.json',
+    project: 'tsconfig.lint.json',
   },
   root: true,
   env: {
@@ -12,6 +12,5 @@ module.exports = {
   plugins: [
     '@typescript-eslint/eslint-plugin',
   ],
-  ignorePatterns: ['.eslintrc.js'],
   // TODO: Add rules, including this one once it's in a plugin
 }
